bfd2947d7d5d0c81f70a0bb437a11d11f854a03f
Term-inator <1121745627@qq.com>
2024-02-25 22:50:56 -0600

    fix:
    cannot find module '.prisma/client/...'

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
-import { app, shell, BrowserWindow } from 'electron'
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { prisma } from './client'
 
 function createWindow(): void {
   // Create the browser window.
@@ -69,3 +70,7 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+// @ts-ignore
+ipcMain.handle('queryUsers', async (event, args) => {
+  return await prisma.user.findMany()
+})
